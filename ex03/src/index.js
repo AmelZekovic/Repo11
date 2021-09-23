5775e46632bc529128d2130f1e0a71d5c1e87bd1
amelzekovic <amelzekovic@gmail.com>
2021-09-24 00:43:46 +0200

    Manipulate array

---
+++ ex03/src/index.js
@@ -0,0 +1,9 @@
+var myPetsArray = ["Dog", "Cat"];
+
+function myArrayFunction(myPets) {
+    var myNewPets = [...myPets];
+    
+}
+
+console.log(myArrayFunction(myNewPets));
+module.exports = myArrayFunction;
